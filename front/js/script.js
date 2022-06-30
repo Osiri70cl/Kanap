d075f679b050feebd286aae7925166c32a084dcb
Remi Pince <remi.pince@outlook.com>
2022-06-30 14:54:58 +0200

    mise en place de l'affichage des produits

---
+++ front/js/script.js
@@ -0,0 +1,39 @@
+"use strict";
+
+const itemsSection = document.getElementById("items");
+
+// Fonction pour créer une nouvelle balise HTML ("exemple") quand la fonction est appelée
+function createTag(newTagName) {
+  return document.createElement(newTagName);
+}
+
+function createProductsCards() {
+  fetch("http://localhost:3000/api/products")
+    .then((response) => {
+      return response.json();
+    })
+    .then((productList) => {
+      productList.forEach((product) => {
+        const productLink = createTag("a");
+        const articleTag = createTag("article");
+        const productImage = createTag("img");
+        const productName = createTag("h3");
+        const productDescription = createTag("p");
+        productLink.appendChild(articleTag);
+        articleTag.append(productImage, productName, productDescription);
+        itemsSection.appendChild(productLink);
+        productLink.href = `./product.html?id=${product._id}`;
+        productImage.src = `${product.imageUrl}`;
+        productImage.alt = `${product.altTxt}`;
+        productName.classList.add("productName");
+        productName.textContent = `${product.name}`;
+        productDescription.classList.add("productDescription");
+        productDescription.textContent = `${product.description}`;
+      });
+    })
+    .catch((err) => {
+      console.log(`error: ${err}`);
+    });
+}
+
+createProductsCards();
