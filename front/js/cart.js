57104bbde106cefbc4a5df4fda0938fa8b725c61
Remi Pince <remi.pince@outlook.com>
2022-07-09 10:54:49 +0200

    avancé du projet et page cart

--- front/js/cart.js
+++ front/js/cart.js
@@ -42,12 +42,53 @@ function displayCart() {
         i++;
       }
       cartItems.innerHTML += cartContent;
+      eventListener();
     });
 }
 
 // Crée un nouveau bloc article pour chaque produit du panier
 const createInnerContent = function () {
-  cartContent += `<article class="cart__item" data-id="${cart[i].id}" data-color="${productColor}"><div class="cart__item__img"><img src="${cartItemImage}" alt="${cartItemImageAlt}"></div><div class="cart__item__content"><div class="cart__item__content__description"><h2>${productName}</h2><p>${productColor}</p><p>${productPrice} €</p></div><div class="cart__item__content__settings"><div class="cart__item__content__settings__quantity"><p>Qté : </p><input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${cart[i].quantity}"></div><div class="cart__item__content__settings__delete"><p class="deleteItem">Supprimer</p></div></div></div></article>`;
+  cartContent += `<article class="cart__item" data-id="${cart[i].id}" data-color="${productColor}">
+  <div class="cart__item__img"><img src="${cartItemImage}" alt="${cartItemImageAlt}"></div>
+  <div class="cart__item__content">
+  <div class="cart__item__content__description">
+  <h2>${productName}</h2>
+  <p>${productColor}</p>
+  <p>${productPrice} €</p>
+  </div>
+  <div class="cart__item__content__settings">
+  <div class="cart__item__content__settings__quantity">
+  <p>Qté : </p>
+  <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${cart[i].quantity}">
+  </div>
+  <div class="cart__item__content__settings__delete">
+  <p class="deleteItem">Supprimer</p>
+  </div>
+  </div>
+  </div>
+  </article>`;
 };
 
 displayCart();
+
+const totalQuantityElement = document.getElementById("totalQuantity");
+const displayedTotal = document.getElementById("totalPrice");
+let removeButton;
+let quantityInputField;
+let getParentArticle;
+let updatedProduct;
+
+const eventListener = function () {
+  quantityInputField = document.querySelectorAll(
+    ".cart__item__content__settings__quantity > .itemQuantity"
+  );
+  quantityInputField.forEach((element) => {
+    element.setAttribute("value", element.value);
+    productQuantity += element.value;
+    element.addEventListener("change", pushLocalStorageQuantity);
+  });
+  removeButton = document.querySelectorAll(".deleteItem");
+  removeButton.forEach((element) => {
+    element.addEventListener("click", removeFromCart);
+  });
+};
